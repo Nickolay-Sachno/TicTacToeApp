445854139892dc08690e8c8cd5ee3dbc8faec5b3
Nickolay-Sachno <nickolay.sachno@is.com>
2022-08-29 10:50:00 +0300

    Adding a network call

--- app/src/main/java/com/example/tictactoe/entry/EntryFragment.kt
+++ app/src/main/java/com/example/tictactoe/entry/EntryFragment.kt
@@ -2,6 +2,7 @@ package com.example.tictactoe.entry
 
 import User
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,8 @@ import com.example.tictactoe.Controller
 import com.example.tictactoe.R
 import com.example.tictactoe.databinding.FragmentEntryBinding
 import com.example.tictactoe.enum.GameType
+import okhttp3.*
+import java.io.IOException
 
 class EntryFragment : Fragment(), IEntryView {
 
@@ -39,6 +42,7 @@ class EntryFragment : Fragment(), IEntryView {
     private fun setClickableButtons(){
         binding.apply {
             playerVsPlayer.setOnClickListener{v : View ->
+                tempCall()
                 Controller.settings.typeGame = GameType.PLAYER_VS_PLAYER
                 Controller.createGameBasedOnTypeGame()
                 v.findNavController().navigate(EntryFragmentDirections.actionEntryFragmentToHostGameScreenFragment())
@@ -51,6 +55,32 @@ class EntryFragment : Fragment(), IEntryView {
             settings.setOnClickListener {v : View ->
                 v.findNavController().navigate(EntryFragmentDirections.actionEntryFragmentToSettingsFragment())
             }
+        }
+    }
+
+    private fun tempCall(){
+        try{
+            val client = OkHttpClient()
+
+            val request = Request.Builder()
+                .url("https://stujo-tic-tac-toe-stujo-v1.p.rapidapi.com/X-O-XO---/X")
+                .get()
+                .addHeader("X-RapidAPI-Key", "c26ff06453msh9930f7571f03f91p154383jsn028a87338af9")
+                .addHeader("X-RapidAPI-Host", "stujo-tic-tac-toe-stujo-v1.p.rapidapi.com")
+                .build()
+
+            client.newCall(request).enqueue(object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        Log.e("ENTRY", "Could not connect ot the server")
+                    }
+
+                    override fun onResponse(call: Call, response: Response) {
+                        Log.i("ENTRY", "Finished with the response:\n${response}")
+                    }
+                }
+            )
+        } finally {
+
         }
     }
 }
